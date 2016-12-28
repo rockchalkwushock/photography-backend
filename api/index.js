c78e1584230c7ef8576f93108d80616f6431348e
Cody Brunner <rockchalkwushock@icloud.com>
2016-12-27 19:00:45 -0600

    Found and fixed server routing error. All routes hit as advertised. User populates with hashed password. JWT is handed out. Auth occurs for GET route. Updated index.html. Updated scripts.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import { Server } from 'http';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors';
+import passport from 'passport';
+import { userRoutes } from './modules';
 
 // Configurations
 import './config/envConfig';
@@ -15,15 +17,28 @@ const mode = process.env.NODE_ENV;
 export const app = express();
 
 // Middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', port);
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials'); // eslint-disable-line
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  return res.status(err.status || port).render('500');
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cors());
+app.use(passport.initialize());
 
 // Routing
-app.get('/api/v1', (req, res) => {
-  res.status(200).json({ version: '1.0.0' });
-});
+app.use('/api/v1', [userRoutes]);
 
 // Express Server
 const server = Server(app);
