855194520e5fa1e8b54b9112422aa1580b970ffd
Cody Brunner <rockchalkwushock@icloud.com>
2016-12-30 11:45:40 -0600

    Added loginUser & logoutUser actionCreators. Added LOGIN & LOGOUT cases in reducer.

--- client/src/modules/auth/actions.js
+++ client/src/modules/auth/actions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { SIGNUP_USER } from './types';
+import { LOGIN_USER, LOGOUT_USER, SIGNUP_USER } from './types';
 
 export const signupUser = values => {
   const { email, password } = values;
@@ -8,13 +8,35 @@ export const signupUser = values => {
   return {
     type: SIGNUP_USER,
     promise: axios.post('/api/v1/signup', { email, password })
-         .then(res => {
-           localStorage.setItem('token', res.data.token); // eslint-disable-line
-           axios.defaults.headers.common['Authorization'] = res.data.token; // eslint-disable-line
-           browserHistory.push('/admin');
-         }),
+     .then(res => {
+       localStorage.setItem('token', res.data.token); // eslint-disable-line
+       axios.defaults.headers.common['Authorization'] = res.data.token; // eslint-disable-line
+       browserHistory.push('/admin');
+     }),
     meta: {
       onFailure: err => console.log(err),
     }
   };
 };
+
+export const loginUser = values => {
+  const { email, password } = values;
+
+  return {
+    type: LOGIN_USER,
+    promise: axios.post('/api/v1/login', { email, password })
+      .then(res => {
+        localStorage.setItem('token', res.data.token); // eslint-disable-line
+        axios.defaults.headers.common['Authorization'] = res.data.token; // eslint-disable-line
+        browserHistory.push('/admin');
+      }),
+    meta: {
+      onFailure: err => console.log(err),
+    }
+  };
+};
+
+export function logoutUser() {
+  localStorage.removeItem('token'); // eslint-disable-line
+  return { type: LOGOUT_USER };
+}
--- client/src/modules/auth/reducers.js
+++ client/src/modules/auth/reducers.js
@@ -1,5 +1,5 @@
 import { handle } from 'redux-pack';
-import { SIGNUP_USER } from './types';
+import { LOGIN_USER, LOGOUT_USER, SIGNUP_USER } from './types';
 
 const initialState = {
   isLoading: false,
@@ -11,6 +11,9 @@ const initialState = {
 export default (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
+    case LOGOUT_USER:
+      return { ...state, authenticated: false };
+    case LOGIN_USER:
     case SIGNUP_USER: return handle(state, action, {
       start: s => ({ ...s, authError: null }),
       failure: s => ({ ...s, authenticated: false, authError: payload }),
