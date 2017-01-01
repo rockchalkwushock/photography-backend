6a188d204ee107fdcbddb74e1a993c78fc3e4306
Cody Brunner <rockchalkwushock@icloud.com>
2016-12-31 20:34:47 -0600

    Added to actiontypes.

--- client/src/modules/auth/types.js
+++ client/src/modules/auth/types.js
@@ -1,4 +1,5 @@
-export const SIGNUP_USER = 'SIGNUP_USER';
+export const CHECK_TOKEN = 'CHECK_TOKEN';
+export const FETCH_MESSAGE = 'FETCH_MESSAGE';
 export const LOGIN_USER = 'LOGIN_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
-export const FETCH_MESSAGE = 'FETCH_MESSAGE';
+export const SIGNUP_USER = 'SIGNUP_USER';
