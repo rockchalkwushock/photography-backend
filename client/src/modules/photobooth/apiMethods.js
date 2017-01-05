d38a68067383faf62b7d4bc3d83b871ccebda613
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-05 15:14:52 -0600

    updated apiMethod for photos.

--- client/src/modules/photobooth/apiMethods.js
+++ client/src/modules/photobooth/apiMethods.js
@@ -4,14 +4,10 @@ export const sendToBackEnd = result => {
   console.log(result);
   const promise = new Promise((resolve, reject) => {
     axios.post('/library', { result })
-      .then(res => {
-        console.log(res);
-        resolve(res);
-      })
-      .catch(err => {
-        console.log(err);
-        reject(err);
-      });
+      .then(
+        res => resolve(res.data.cloudinary),
+        err => reject(err)
+      );
   });
   return promise;
 };
