dd5e500b93e98d496617c1f8f0d3858754a111e8
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-05 15:15:22 -0600

    Wrote controller for GET, added controller to GET route.

--- api/modules/Photo/controller.js
+++ api/modules/Photo/controller.js
@@ -3,8 +3,11 @@ import Photo from './model';
 
 export const getPublicID = (req, res) => {
   const data = req.body.result; // this should be an array of object(s)
+  console.log('REQUEST FROM FRONT-END');
+  console.log('-----------------------');
   console.log(data);
-  if (!data) res.status(422).json({ success: false, message: 'Upload Failed!' });
+  console.log('-----------------------');
+  if (data === undefined) res.status(422).json({ success: false, message: 'Upload Failed!' });
   console.time('ARRAY');
   const insertStuff = data.reduce((array, item) => {
     array.push({
@@ -26,4 +29,21 @@ export const getPublicID = (req, res) => {
       }),
       err => res.status(422).json({ success: false, message: err })
     );
+};
+
+export const sendToFrontEnd = (req, res) => {
+  /* TODO: request from front-end should ask for:
+      1) All images.
+      2) Active images.
+      3) Images in order:
+        a) Newest to Oldest
+        b) Oldest to Newest
+  */
+  console.time('ARRAY');
+  Photo.find()
+    .then(
+      img => res.status(201).json({ success: true, payload: img }),
+      err => res.status(500).json({ message: err })
+    );
+    console.timeEnd('ARRAY');
 };
--- api/modules/Photo/routes.js
+++ api/modules/Photo/routes.js
@@ -5,6 +5,6 @@ import { requireAuth } from '../../helpers/authHelpers';
 const routes = new Router();
 
 routes.route('/library').post(requireAuth, PhotoController.getPublicID);
-routes.route('/library').get(requireAuth);
+routes.route('/library').get(requireAuth, PhotoController.sendToFrontEnd);
 
 export default routes;
