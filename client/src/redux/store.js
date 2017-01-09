57ce7f6698d19a79f97e40492ca3f803127e0d7e
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-09 12:39:32 -0600

    removed redux-logger

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,26 +1,22 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import { middleware as reduxPackMiddleware } from 'redux-pack';
 import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 import promise from 'redux-promise-middleware';
 import { throttle } from 'lodash';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import createLogger from 'redux-logger';
 import { loadState, saveState } from '../helpers/localStorage';
 import rootReducer from './reducers';
 
 const routingMiddleware = routerMiddleware(browserHistory);
 const persistedState = loadState();
 const middlewares = [promise(), routingMiddleware, reduxPackMiddleware];
-const middlewareDev = [createLogger()];
 let enhancers;
 
 if (process.env.NODE_ENV !== 'production') {
-  enhancers = composeWithDevTools(
-    applyMiddleware(...middlewares, ...middlewareDev)
-  );
-} else {
   enhancers = composeWithDevTools(applyMiddleware(...middlewares));
+} else {
+  enhancers = compose(applyMiddleware(...middlewares));
 }
 
 const store = createStore(rootReducer, persistedState, enhancers);
