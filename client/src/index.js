a749dbae4dfc14a579777031b17d55f6d55ba13c
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-08 20:06:44 -0600

    added react-redux-toastr & initial code base.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr';
 import { store } from './redux/store';
 import Routes from './Routes';
 
 render(
   <Provider store={store}>
+    <div>
       <Routes />
+      <ReduxToastr
+        timeOut={4000}
+        newestOnTop={false}
+        preventDuplicates
+        position="top-right"
+        transitionIn="bounceIn"
+        transitionOut="bounceOut"
+        progressBar
+      />
+    </div>
   </Provider>,
   document.getElementById('root')
 );
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { reducer as toastrReducer } from 'react-redux-toastr';
 import { reducer as formReducer } from 'redux-form';
 import { authReducer, photoReducer } from '../../modules';
 
@@ -7,5 +8,6 @@ export default combineReducers({
   auth: authReducer,
   form: formReducer,
   photo: photoReducer,
-  routing: routerReducer
+  routing: routerReducer,
+  toastr: toastrReducer
 });
