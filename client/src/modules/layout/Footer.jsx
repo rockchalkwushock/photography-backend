33283ac13389f38b2b6c3a526be3536ae09b4082
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-09 09:21:50 -0600

    Added Footer.jsx. Updated import/export.

---
+++ client/src/modules/layout/Footer.jsx
@@ -0,0 +1,33 @@
+import React, { PropTypes } from 'react';
+
+const styles = {
+  root: {
+    auth: {
+      backgroundColor: '#2185D0',
+      color: 'white',
+      fontStyle: 'italic',
+      fontWeight: 'bold',
+    },
+    backgroundColor: '#FFF',
+    color: 'black',
+    fontSize: '1em'
+  }
+};
+
+const Footer = ({ auth }) => (
+  <footer style={!auth ? styles.root : styles.root.auth}>
+    <div className="containter" style={{ padding: '0.5em' }}>
+      <ul>
+        <li>Name</li>
+        <li>Contact</li>
+        <li>Copyright</li>
+      </ul>
+    </div>
+  </footer>
+);
+
+Footer.propTypes = {
+  auth: PropTypes.object,
+};
+
+export default Footer;
--- client/src/modules/layout/index.js
+++ client/src/modules/layout/index.js
@@ -1,5 +1,6 @@
+import Footer from './Footer';
 import HomePage from './HomePage';
 import Navbar from './Navbar';
 import Page404 from './Page404';
 
-export { HomePage, Navbar, Page404 };
+export { Footer, HomePage, Navbar, Page404 };
