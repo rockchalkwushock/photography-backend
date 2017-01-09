ea1ca9780df4fa3b162d4dd604b9ef140b767743
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-09 09:56:30 -0600

    updated auth-wrapper

--- client/src/modules/auth/wrapper/AuthViews.jsx
+++ client/src/modules/auth/wrapper/AuthViews.jsx
@@ -3,11 +3,11 @@ import { routerActions } from 'react-router-redux';
 import Loading from 'react-loading';
 
 const UserIsAuthenticated = UserAuthWrapper({
-  authSelector: state => state.auth, // how to get the user state
+  authSelector: state => state.auth,
   authenticatingSelector: state => state.auth.isLoading,
   LoadingComponent: Loading,
-  redirectAction: routerActions.replace, // the redux action to dispatch for redirect
-  wrapperDisplayName: 'UserIsAuthenticated' // a nice name for this auth check
+  redirectAction: routerActions.replace,
+  wrapperDisplayName: 'UserIsAuthenticated'
 });
 
 const AuthViews = UserIsAuthenticated(({ children }) => children);
--- client/src/modules/auth/wrapper/NonAuthViews.jsx
+++ client/src/modules/auth/wrapper/NonAuthViews.jsx
@@ -1,11 +1,9 @@
 import { UserAuthWrapper } from 'redux-auth-wrapper';
-import Loading from 'react-loading';
 
 const VisibleOnlyNoUser = UserAuthWrapper({
   authSelector: state => state.auth,
-  LoadingComponent: Loading,
   wrapperDisplayName: 'VisibleOnlyIfNotUser',
-  predicate: auth => !auth.authenticated,
+  predicate: auth => !auth.token,
   failureRedirectPath: '/'
 });
 
