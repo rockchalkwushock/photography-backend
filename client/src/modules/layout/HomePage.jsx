4f608cbcecd9fc9e0c0be935b348dd6beb146bd2
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-13 13:29:28 -0600

    Added flags & locale change onClick.

--- client/src/modules/layout/HomePage.jsx
+++ client/src/modules/layout/HomePage.jsx
@@ -1,8 +1,20 @@
 import React, { PropTypes } from 'react';
-import { withTranslate } from 'react-redux-multilingual';
+import { IntlActions, withTranslate } from 'react-redux-multilingual';
+import { Flag } from 'semantic-ui-react';
+import { store } from '../../redux/store';
 
 const HomePage = ({ translate }) => (
   <div className='homepage'>
+    <div className="flags">
+      <Flag
+        name='ru'
+        onClick={() => store.dispatch(IntlActions.setLocale('ru'))}
+      />
+      <Flag
+        name='us'
+        onClick={() => store.dispatch(IntlActions.setLocale('en'))}
+      />
+    </div>
     <h1>{translate('home:h1')}</h1>
     <h2>{translate('home:h2')}</h2>
     <h3>{translate('home:h3')}</h3>
