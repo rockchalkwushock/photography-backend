dae23d3aabecb868cea37806b3ad0a6d4d8bff13
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-27 10:21:46 -0600

    API server connected & ready for deploy.

---
+++ api/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import { Server } from 'http';
+import path from 'path';
+import { dbConfig, middlewaresConfig } from './config';
+
+const app = express();
+
+// ENVIRONMENT VARIABLES
+const MODE = process.env.NODE_ENV;
+export const PORT = process.env.PORT || 3000;
+
+// MIDDLEWARE
+middlewaresConfig(app);
+
+let mongoConf;
+
+// NODE ENVIRONMENT SETUP
+switch (MODE) {
+  case 'production': {
+    app.use(express.static('dist'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../dist/index.html'));
+    });
+    mongoConf = process.env.MONGO_URI;
+    break;
+  }
+  case 'test': {
+    mongoConf = 'mongodb://localhost/photography-dev';
+    break;
+  }
+  default: {
+    mongoConf = 'mongodb://localhost/photography-dev';
+    break;
+  }
+}
+
+// DATABASE
+dbConfig(mongoConf);
+
+// EXPRESS SERVER
+export const server = Server(app);
+server.listen(PORT, err => {
+  if (err) { return console.error(err); }
+  console.log(`App running to port: ${PORT} in ${MODE}`);
+});
