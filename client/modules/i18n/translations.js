2b191686c91ce5ba41c10ab4314b137d1d1b0eac
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-27 11:02:55 -0600

    Added i18n module.

---
+++ client/modules/i18n/translations.js
@@ -0,0 +1,10 @@
+import transen from './locales/locale-en';
+import transru from './locales/locale-ru';
+
+const trans = {
+  en: transen,
+  ru: transru
+};
+
+export const translate = trans;
+export default trans;
